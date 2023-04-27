76589ec8aa2a79277d03e838c7bca035877e4ec6
Akshat Jaimini <destrex271@gmail.com>
2023-04-27 15:01:51 +0530

    lst

---
+++ last.ino
@@ -0,0 +1,152 @@
+int leftW;
+int rightW;
+int echo = 12;
+int trigger = 13;
+int gantryCounter = 0;
+long int echPulse;
+int prevTime = 0;
+int count = 0;
+int pin = 4;
+int timeCur;
+int flag = 0  ;
+int ctr = 0;
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(A2, INPUT); //right IR sensor
+  pinMode(A0, INPUT); //left IR sensor
+  pinMode(pin, INPUT);
+  pinMode(echo, INPUT);
+  pinMode(pin, INPUT);
+  pinMode(trigger, OUTPUT);
+  digitalWrite(pin , LOW);
+  for (int i = 5; i < 9; i++) {
+    pinMode(i, OUTPUT);
+  }
+  timeCur = 0;
+}
+void stopW(){
+  digitalWrite(5, LOW);
+  digitalWrite(6, LOW);
+  digitalWrite(7, LOW);
+  digitalWrite(8, LOW);
+}
+void forward()
+{
+  digitalWrite(5, HIGH);
+  digitalWrite(6, LOW);
+  digitalWrite(7, LOW);
+  digitalWrite(8, HIGH);
+}
+void left()
+{
+  digitalWrite(5, HIGH);
+  digitalWrite(6, LOW);
+  digitalWrite(7, LOW);
+  digitalWrite(8, LOW);
+}
+void right()
+{
+  digitalWrite(5, LOW);
+  digitalWrite(6, LOW);
+  digitalWrite(7, LOW);
+  digitalWrite(8, HIGH);
+}
+void loop() {
+  timeCur = millis();
+  leftW = digitalRead(A2);
+  rightW = digitalRead(A0);
+  int rec = digitalRead(pin);
+//  digitalWrite(trigger, LOW);
+//  delayMicroseconds(2);
+//  digitalWrite(trigger, HIGH);
+//  delayMicroseconds(10);
+//  digitalWrite(trigger, LOW); 
+//  int ult = digitalRead(echo);
+//  if (ult == HIGH){
+//    echPulse = pulseIn(echo,HIGH);
+//    float distance = (echPulse * 0.0343) / 2;
+//    if(distance == 20){
+//      stopW();
+//    }
+//  }
+  if(rec == HIGH){
+    Serial.println("IN REC");
+    ctr = (ctr + 1)%3;
+    int StartTime = millis();
+   int d = pulseIn(pin,HIGH);
+   if(d > 2000 and d < 3000){
+      gantryCounter=gantryCounter+1;
+      Serial.println("Gantry: 1 ");
+      stopW();
+      delay(1000);
+    }
+    else if (d> 500 and d < 1000){
+          gantryCounter=gantryCounter+1;
+          Serial.println("Gantry: 2");
+          stopW();
+          delay(1000);
+      }
+    else if (d > 3000 and d < 4000){
+     // Serial.println(d);
+     
+      Serial.println("Gantry: 3");
+      gantryCounter=gantryCounter+1;
+      Serial.print("The gantry Counter is: ");
+      Serial.println(gantryCounter);
+      stopW();
+      delay(1000);
+  
+    }
+   else {
+       //Serial.println(d);
+       stopW();
+       delay(1000);
+       Serial.println("Passed Gantry!!");
+       
+   }
+  }
+  if(Serial.available() > 0 || flag == 0){
+    char s = Serial.read();
+    if(s == 'K'){
+      flag = 1;
+    }
+  }
+  if(flag == 1){
+    if (leftW == 1 && rightW == 1) {
+      forward();
+    }
+    else if (leftW == 1 && rightW == 0) {
+      right();
+    }
+    else if (leftW == 0 && rightW == 1) {
+      left();
+    }
+    else {
+//      Serial.println("BLACK");
+      if(timeCur - prevTime >= 1000){
+        count++;
+        Serial.println("BLACK-BLACK "+ count);
+        prevTime = timeCur;
+      }
+      if(count == 4){
+        right();
+//        count = 0;
+        delay(700);
+      }
+      if(count == 9){
+        forward();
+        stopW();
+        right();
+        delay(700);
+      }
+      if (count == 11){
+        stopW();
+        exit(0);
+      }
+      else{
+        forward();
+      }
+    }
+  }
+  }
